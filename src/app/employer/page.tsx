e24fa1db0bb9564a8df2df5b0577c6d0ec2a2c13
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-09 14:05:49 +0500

    added employer

--- src/app/employer/page.tsx
+++ src/app/employer/page.tsx
@@ -1,8 +1,28 @@
-export default function Home() {
-  let arr = ["", "", "", ""];
+import { Button } from "@nextui-org/react";
+import { Metadata } from "next";
+import React from "react";
+import LogoKor from "../components/Logo";
+export const metadata: Metadata = {
+  title: "KOR TJ - Работодателям",
+  description: "Разместите вакансию на KOR TJ",
+};
+export default function Employer() {
   return (
-    <main className="w-full overflow-auto p-4 grid gap-3 grid-cols-5 grid-rows-2">
-      Employer
+    <main className="w-full h-full flex justify-center flex-col gap-5 items-center">
+      <div className="text-5xl text-primary flex gap-3 flex-col text-center justify-center items-center">
+        Разместите вакансию на {" "}
+        <div className="text-black flex gap-2 justify-center items-center" ><LogoKor /> KOR TJ</div>
+      </div>
+      <Button className="bg-black text-white" variant="shadow">
+        Разместить ваканцию
+      </Button>
+      {/* <Card className="w-1/2 h-1/2">
+        <CardHeader className="flex gap-3"></CardHeader>
+        <Divider />
+        <CardBody></CardBody>
+        <Divider />
+        <CardFooter></CardFooter>
+      </Card> */}
     </main>
   );
 }
