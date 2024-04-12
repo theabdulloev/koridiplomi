59ef4b740dc1681ce384f617108eb25d49466283
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-12 16:23:17 +0500

    telegram bot

--- src/app/api/telegrambot.ts
+++ src/app/api/telegrambot.ts
@@ -3,6 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import bot from "../../telegrambot/telegram"; // Путь к вашему файлу telegramBot.ts
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  bot.sendMessage("CHAT_ID", "Привет из Next.js!"); // Замените CHAT_ID на ID чата
+  bot.sendMessage('', "Привет из Next.js!"); // Замените CHAT_ID на ID чата
   res.status(200).json({ message: "Сообщение отправлено!" });
 };
