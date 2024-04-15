a49b6d715c0c1f7a86f8b5a4979f9c0f5c04ed0e
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-15 15:02:59 +0500

    bto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,69 @@ async function getData() {
   return res.json();
 }
 export default async function Home() {
-  const data = await getData();
-
+  // const data = await getData();
+  const data = [
+    {
+      userName: "user5481",
+      vip: true,
+      fullName: "John Doe",
+      description:
+        "Backend developer and data science enthusiast. Let's explore the world of algorithms together!",
+      watch: "1025",
+      salary: "5 000",
+      contacts: "+992 92 999 99 99",
+      email: "email@email.com",
+      location: "Khujand",
+    },
+    {
+      userName: "user9023",
+      vip: true,
+      fullName: "Alice Smith",
+      description:
+        "Mobile app developer and technology geek. Join me in the world of mobile innovation!",
+      watch: "1897",
+      salary: "12 000",
+      contacts: "+992 92 999 99 99",
+      email: "email@email.com",
+      location: "Khujand",
+    },
+    {
+      userName: "user17653",
+      vip: false,
+      fullName: "Emma Johnson",
+      description:
+        "Game developer and virtual reality explorer. Embark on an epic gaming journey with me!",
+      watch: "3245",
+      salary: "2 700",
+      contacts: "+992 92 999 99 99",
+      email: "email@email.com",
+      location: "Khujand",
+    },
+    {
+      userName: "user17652",
+      vip: false,
+      fullName: "Emma Johnson",
+      description:
+        "Game developer and virtual reality explorer. Embark on an epic gaming journey with me!",
+      watch: "3245",
+      salary: "42 700",
+      contacts: "+992 92 999 99 99",
+      email: "email@email.com",
+      location: "Khujand",
+    },
+    {
+      userName: "user17651",
+      vip: false,
+      fullName: "Emma Johnson",
+      description:
+        "Game developer and virtual reality explorer. Embark on an epic gaming journey with me!",
+      watch: "3245",
+      salary: "1 700",
+      contacts: "+992 92 999 99 99",
+      email: "email@email.com",
+      location: "Khujand",
+    },
+  ];
   return (
     <main>
       <div className="p-5 text-center uppercase text-2xl">
