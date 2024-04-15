eb2a05b1b7da239b09e1e57bc39495c0d773a549
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-15 15:06:17 +0500

    bto

--- src/telegrambot/bot.js
+++
@@ -1,7 +0,0 @@
-import TeleBot from "telebot";
-
-const bot = new TeleBot("6914168476:AAEyNpmUF1TkDf6E-I5m4eccVbvZ23T94Kw");
-
-bot.on("text", (msg) => msg.reply.text(msg.text));
-
-export default bot;
