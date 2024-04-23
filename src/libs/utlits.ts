d75d3fed17b5359adba16dad912f93dcafd6cf01
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-23 16:02:32 +0500

    fixed mongodb base

--- src/libs/utlits.ts
+++ src/libs/utlits.ts
@@ -1,10 +1,10 @@
 import { env } from "process";
 import { Vacancies } from "./models";
 import mongoose from "mongoose";
-// mongodb+srv://abdulloev:0CSxryh0nWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/?retryWrites=true&w=majority
+// 
 async function main() {
   try {
-    await mongoose.connect('mongodb+srv://abdulloev:0CSxryh0nWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://abdulloev:0CSxryhOnWJEhipW@tojikonbookcl.jsrcsga.mongodb.net/?retryWrites=true&w=majority');
     console.log("mongodb connected");
     // const testCol = mongoose.db().collection()
     // await client.db().createCollection('B')
