df10d7dcf363e1cb6c52b7dcd809937758b34c3c
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-25 16:34:11 +0500

    api instagram

--- src/app/instagram/page.tsx
+++ src/app/instagram/page.tsx
@@ -3,17 +3,14 @@ import publicInstagram from "@/libs/instagram";
 import { Button } from "@nextui-org/react";
 
 export default async function Instagram() {
+  const get = async () => {
+    console.log("...start public");
+    const data = await publicInstagram();
+    console.log(data);
+  };
   return (
     <>
-      <Button
-        onClick={async () => {
-          console.log("...start public");
-          const data = await publicInstagram();
-          console.log(data);
-        }}
-      >
-        +
-      </Button>
+      <Button onClick={async () => await get()}>+</Button>
     </>
   );
 }
