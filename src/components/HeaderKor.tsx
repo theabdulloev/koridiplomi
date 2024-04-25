1de86b21d3d9f6954a88ca4ba17e75b2c52bbf16
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-04-25 16:22:08 +0500

    api instagram

--- src/components/HeaderKor.tsx
+++ src/components/HeaderKor.tsx
@@ -98,7 +98,7 @@ export default function App() {
                 const data = publicInstagram();
                 console.log(data)
               }}
-            ></Button>
+            >+</Button>
           </NavbarItem>
         </NavbarContent>
 
