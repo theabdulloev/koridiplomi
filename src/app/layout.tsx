c945cbc7329fbf0303287d06327d3b92ecb37db7
Ismoiljon Abdulloev <110225249+theabdulloev@users.noreply.github.com>
2024-05-01 01:29:05 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,13 @@ export default function RootLayout({
           name="google-site-verification"
           content="TY52qt7axMYAHDvC9xNbeZvTQlngPkvE6wWR5uzdtqk"
         />
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NQ2LB9VM');</script>
+<!-- End Google Tag Manager -->
         <ColorSchemeScript />
       </head>
       <body className="w-screen  flex flex-col overflow-x-hidden">
