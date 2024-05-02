e5bf0e8c18090ca7306e05000813733c00ddf3c0
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-05-02 13:49:13 +0500

    new data from mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 import KORTJ from "@/components/comm";
 import Filter from "../components/FilterCompanent";
 import SearcCompanent from "@/components/Search";
-import { GetData } from "@/libs/utlits";
 import ToPop from "@/components/totop";
 
+async function getData() {
+  const res = await fetch("https://ruznomatj.vercel.app/products", {
+    cache: "no-store",
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
 export default async function Home() {
-  const data: [] = await GetData();
+  const data: [] = await getData();
 
   return (
     <main>
