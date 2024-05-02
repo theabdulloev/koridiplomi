86ca2d0a20b35f56e736e28c9ea6488dfa394a05
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-05-02 14:57:21 +0500

    intervale

--- src/app/mongodb/page.tsx
+++ src/app/mongodb/page.tsx
@@ -1,4 +1,5 @@
 "use server"
+import save from "@/libs/save";
 import { Button } from "@nextui-org/react";
 
 async function getData() {
@@ -15,32 +16,7 @@ async function getData() {
 
   return res.json();
 }
-const save = async () => {
-  const data = {
-    username: "test",
-    vip: true,
-    fullName: "Ismoiljon Abdulloev",
-    description:
-      "Мутахассис оид ба тасдиқи амалиёти бонкӣ, филиал дар ш.Хуҷанд",
-    watch: 189,
-    salary: 12000,
-    contacts: "+992 97 809 23 23",
-    email: "ismoiljon.abdulloev.ibt@gmail.com",
-    location: "Хуҷанд",
-    verification: true,
-  };
-  try {
-    const res = await fetch("https://ruznomatj.vercel.app/products", {
-      method: "POST",
-      body: JSON.stringify(data),
-    });
-    const result = await res.json();
 
-    return Response.json(result);
-  } catch (error) {
-    console.log(error);
-  }
-};
 export default async function Mongo() {
   const data = await getData();
   console.log(data);
