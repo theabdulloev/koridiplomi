b586bc665fc597ce3b29ab476fd2a6d2c6f7db6f
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-05-03 17:03:11 +0500

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://bekor.vercel.app/",
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 1,
+        },
+        {
+            url: "https://bekor.vercel.app/employer",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+    ];
+}
