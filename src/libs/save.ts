935c5fd77b169ce52b9f6dc09549dc9b3d654762
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-05-04 16:28:33 +0500

    test

--- src/libs/save.ts
+++ src/libs/save.ts
@@ -3,7 +3,13 @@ export default async function PostVacancies(data: any) {
     console.log(data, "from server");
     const result = await axios.post(
         "https://ruznomatj.vercel.app/products",
-        data
+        data,
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
     );
+
     return result;
 }
